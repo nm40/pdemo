0299bcabe5432e55a747b972eb0b02663bfa4e6d
nm40 <mirwani40@gmail.com>
2021-07-08 14:36:47 +0530

    addedfn header for all fn

--- math.py
+++ math.py
@@ -1,11 +1,12 @@
+#Add implementation
 def add(x,y):
 	pass
-
+#Subtrat implementation
 def subtract(x,y):
 	pass
-
+#Multiply implementation
 def mutiply(x,y):
 	pass
-
+#Divide implementation
 def divide(x,y):
 	pass
