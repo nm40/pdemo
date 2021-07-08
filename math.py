817f5a542bbdeaefec6d049482fb40c83f94e881
nm40 <60837448+nm40@users.noreply.github.com>
2021-07-08 14:28:40 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#Add implementation
+def add(x,y):
+	return x+y
+#subtract implementation
+def subtract(x,y):
+	return x-y
+#multiply implementation
+def mutiply(x,y):
+	return x*y
+#divide implementation
+def divide(x,y):
+	if y==0:
+		return error
+	else:
+		return x/y
